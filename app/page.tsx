725df02bc7aaef92dd641660aa018c14ecf24bf8
turko-dev <turkoai@protonmail.com>
2026-08-24 08:43:18 +0100

    added charts to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Section from "@/components/section";
 
 import { Marker, MarkerContent, MarkerIcon } from "@/components/ui/marker"
 import { SearchIcon } from "lucide-react";
+import { DummyChart1 } from "@/components/dummychart1";
+import { DummyChart2 } from "@/components/dummychart2";
 
 
 export default function Home() {
@@ -16,15 +18,18 @@ export default function Home() {
           <HeroForm></HeroForm>
           <Section direction="row">
             <div className="flex w-full flex-col gap-12 py-12">
-            <Marker>
+            <Marker className="shimmer">
               <MarkerContent><h1 className="font-mono text-lg">So, I can shorten thousands of links. All for free?</h1></MarkerContent>
             </Marker>
-            <Marker variant="separator">
+            <Marker className="shimmer" variant="separator">
               <MarkerIcon><SearchIcon /></MarkerIcon>
               <MarkerContent className="font-mono text-md">Up to 2,400 URLs</MarkerContent>
             </Marker>
-            <div className="min-h-screen flex w-full flex-col gap-4 p-4 bg-primary rounded-xl"></div>
-            
+            <div className="min-h-fit w-full grid grid-cols-2  gap-4">
+             <DummyChart1></DummyChart1>
+             <DummyChart2></DummyChart2>
+
+            </div>
             </div>
 
           </Section>
