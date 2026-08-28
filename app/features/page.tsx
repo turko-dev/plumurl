a8aacc40a24a238a8309638532461ea9cbefcc80
turko-dev <turkoai@protonmail.com>
2026-08-28 08:05:31 +0100

    added features intro

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,22 +1,5 @@
-import PlumSidebar from "@/components/plum-sidebar";
-import Navbar from "@/components/navbar";
-import Page from "@/components/page";
-import PageContent from "@/components/pagecontent";
-
-
+import { redirect } from "next/navigation";
 // WARNING - THIS PAGE IS NOT FOR USE, REDIRECT TO /introduction ALWAYS
-
-
 export default function Features() {
-    return(
-        <Page>
-            {/* Navbar */}
-            <Navbar />
-            
-            <PageContent>
-                <PlumSidebar route="/features" />
-            </PageContent>
-
-        </Page>
-    )
+    return redirect("/features/introduction")
 }
--- components/plum-sidebar.tsx
+++ components/plum-sidebar.tsx
@@ -14,38 +14,15 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
-export default function PlumSidebar({route}: {route: string}) {
+export default function PlumSidebar({route, children}: {route: string, children: React.ReactNode}) {
    
 
 
   return (
     <SidebarProvider>
       <AppSidebar route={route} />
-      <SidebarInset>
-        {/* <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header> */}
-        <div className="flex mt-14 flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
-      </SidebarInset>
+      {children}
+     
     </SidebarProvider>
   )
 }
