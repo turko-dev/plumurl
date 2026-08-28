ee00c29685e916c72eb69e2b43cf33ccd3c2ef4e
turko-dev <turkoai@protonmail.com>
2026-08-28 09:08:28 +0100

    new sidebar

---
+++ app/getting-started/introduction/page.tsx
@@ -0,0 +1,13 @@
+import Page from "@/components/page";
+import PageContent from "@/components/pagecontent";
+import ResourcesDashboard from "@/components/resources-dashboard";
+
+export default function GettingStartedIntroduction() {
+    return(
+        <Page>
+            <ResourcesDashboard route="/getting-started/introduction">
+            
+            </ResourcesDashboard>
+        </Page>
+    )
+}
