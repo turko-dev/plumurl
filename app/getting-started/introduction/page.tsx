668f28555f6921e3d47dad116e3adb9c49ff0d17
turko-dev <turkoai@protonmail.com>
2026-08-28 08:05:52 +0100

    missed one edit

--- app/getting-started/introduction/page.tsx
+++ app/getting-started/introduction/page.tsx
@@ -3,6 +3,7 @@ import PlumSidebar from "@/components/plum-sidebar";
 import Navbar from "@/components/navbar";
 import Page from "@/components/page";
 import PageContent from "@/components/pagecontent";
+import { SidebarInset } from "@/components/ui/sidebar";
 
 
 export default function GettingStartedIntroduction() {
@@ -10,7 +11,18 @@ export default function GettingStartedIntroduction() {
             <Page>
                 <Navbar />
                 <PageContent>
-                    <PlumSidebar route="/getting-started/introduction" />
+                    <PlumSidebar route="/getting-started/introduction">
+                         <SidebarInset>
+                            <div className="flex mt-14 flex-1 flex-col gap-4 p-4">
+                            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                                <div className="aspect-video rounded-xl bg-muted/50" />
+                                <div className="aspect-video rounded-xl bg-muted/50" />
+                                <div className="aspect-video rounded-xl bg-muted/50" />
+                            </div>
+                            <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                            </div>
+                        </SidebarInset>
+                    </PlumSidebar>
                 </PageContent>
             </Page>
     )
