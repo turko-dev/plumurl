e90405febac012e13c2d263c053a33870c358a21
turko-dev <turkoai@protonmail.com>
2026-08-29 09:47:52 +0100

    created skeletons for dashboard pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,26 @@
+import { DashboardCardSkeleton } from "@/components/dashboard-card-skeleton";
+import { DashboardCardSkeletonAlt } from "@/components/dashboard-card-skeleton-alt";
 import DashboardMenu from "@/components/dashboard-menu";
 
 export default function Dashboard() {
   return(
       <DashboardMenu route={"/dashboard"}>
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="grid grid-cols-3 gap-4 md:grid-cols-6">
+              <DashboardCardSkeleton />
+              <DashboardCardSkeleton />
+              <DashboardCardSkeleton />
+              <DashboardCardSkeleton />
+              <DashboardCardSkeleton />
+              <DashboardCardSkeleton />
           </div>
-          <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="grid auto-rows-min gap-4 h-full md:grid-cols-2">
+            <DashboardCardSkeleton />
+            <div className="flex flex-row md:flex-row h-full gap-4">
+              <DashboardCardSkeletonAlt />
+              <DashboardCardSkeletonAlt />
+            </div>
+          </div>
+
       </DashboardMenu>
   )
 }
