9a9ca566a4dfbc51aa0440ec500f0c73b4fa5959
turko-dev <turkoai@protonmail.com>
2026-08-29 19:48:50 +0100

    UI for interactive chart done

--- app/dashboard/kpi-all-time-clicks.tsx
+++ app/dashboard/kpi-all-time-clicks.tsx
@@ -1,158 +1,255 @@
 "use client"
 
+import {
+  ArchiveIcon,
+  ArrowLeftIcon,
+  CalendarPlusIcon,
+  CircleQuestionMark,
+  ClockIcon,
+  ListFilterIcon,
+  MailCheckIcon,
+  MoreHorizontalIcon,
+  TagIcon,
+  Trash2Icon,
+} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { ButtonGroup, ButtonGroupSeparator } from "@/components/ui/button-group"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 import * as React from "react"
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import {Card,CardContent,CardDescription,CardHeader,CardTitle,} from "@/components/ui/card"
 import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
   type ChartConfig,
 } from "@/components/ui/chart"
-
-export const description = "An interactive line chart"
-
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
 type KPIAllTimeClicksConfig = {
-  date: Date,
+  date: string,
   allTimeClicks: number
 }
-
-
-
-
-const chartData = [
-  { date: "2023-12-01", allTimeClicks: 0 },
-  { date: "2024-01-01", allTimeClicks: 0 },
-  { date: "2024-02-01", allTimeClicks: 0 },
-  { date: "2024-03-01", allTimeClicks: 0 },
-  { date: "2024-04-01", allTimeClicks: 0 },
-  { date: "2024-06-01", allTimeClicks: 0 },
-  { date: "2024-05-01", allTimeClicks: 0 },
-  { date: "2024-07-01", allTimeClicks: 0 },
-  { date: "2024-08-01", allTimeClicks: 0 },
-  { date: "2024-09-01", allTimeClicks: 0 },
-  { date: "2024-10-01", allTimeClicks: 0 },
-  { date: "2024-11-01", allTimeClicks: 0 },
-  { date: "2024-12-01", allTimeClicks: 0 },
-]
+export const description = "A KPI card showing "
 
 const chartConfig = {
   views: {
-    label: "All Clicks",
+    label: "All Time Clicks",
   },
   allTimeClicks: {
-    label: "All Clicks",
+    label: "All Time Clicks",
     color: "var(--chart-1)",
   }
 } satisfies ChartConfig
 
-export function KPIAllTimeClicks({data}: {data: KPIAllTimeClicksConfig | null}) {
-  const [activeChart, setActiveChart] =
-    React.useState<keyof typeof chartConfig>("allTimeClicks")
 
-  const total = React.useMemo(
-    () => (
+//Function Export
+export function KPIAllTimeClicks({chartData}: {chartData: KPIAllTimeClicksConfig[]}) {
 
-    {    
-      allTimeClicks: chartData.reduce((acc, curr) => 
-        
-        acc + curr.allTimeClicks, 0
+    const date = {
+        day: new Date().getDate().toString(),
+        month: new Date().getMonth().toString(),
+        year: new Date().getFullYear().toString()
+    }
+
+    //ActiveChart State
+    const [activeChart, setActiveChart] = React.useState<keyof typeof chartConfig>("allTimeClicks")
     
-    ),
-    }),
-    []
-  )
+    const [label, setLabel] = React.useState("personal")
+    
 
-  return (
-    <Card className="py-4 sm:py-0">
-      <CardHeader className="flex flex-col items-stretch border-b p-0! sm:flex-row">
-        <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
-          <CardTitle>Line Chart - Interactive</CardTitle>
-          <CardDescription>
-            Showing total visitors for the last 3 months
-          </CardDescription>
-        </div>
-        <div className="flex">
-          {["allTimeClicks"].map((key) => {
-            const chart = key as keyof typeof chartConfig
-            return (
-              <button
-                key={chart}
-                data-active={activeChart === chart}
-                className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-t-0 sm:border-l sm:px-8 sm:py-6"
-                onClick={() => setActiveChart(chart)}
-              >
-                <span className="text-xs text-muted-foreground">
-                  {chartConfig[chart].label}
-                </span>
-                <span className="text-lg leading-none font-bold sm:text-3xl">
-                  {total[key as keyof typeof total].toLocaleString()}
-                </span>
-              </button>
-            )
-          })}
-        </div>
-      </CardHeader>
-      <CardContent className="px-2 sm:p-6">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
-        >
-          <LineChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              left: 12,
-              right: 12,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              minTickGap={32}
-              tickFormatter={(value) => {
-                const date = new Date(value)
-                return date.toLocaleDateString("en-US", {
-                  month: "short",
-                  year:"numeric"
-                })
-              }}
-            />
-            <ChartTooltip
-              content={
-                <ChartTooltipContent
-                  className="w-[150px]"
-                  nameKey="views"
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                      year: "numeric",
+
+    //Total Reduction of allTimeClicks data
+    const total = React.useMemo(() => ({ allTimeClicks: chartData.reduce((acc, curr) => acc + curr.allTimeClicks, 0),}),[])
+
+
+    return (
+        <Card className="py-4 sm:py-0">
+        <CardHeader className="flex flex-col items-stretch border-b p-0! sm:flex-row">
+            <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
+            <CardTitle>Raw All Clicks Data</CardTitle>
+            <CardDescription>
+                Showing total visitors for the past year
+            </CardDescription>
+            </div>
+
+            
+
+            
+            <div className="flex flex-col sm:flex-row gap-4 sm:justify-center justify-baseline items-stretch">
+<ButtonGroup className="p-4 max-sm:w-full sm:justify-center items-center justify-between">
+      <ButtonGroup className="hidden sm:flex">
+        
+        <HoverCard>
+          <HoverCardTrigger delay={100} closeDelay={100} render={<Button variant="outline" size="icon" aria-label="">
+          <CircleQuestionMark/>
+        </Button>} />
+          <HoverCardContent side={"bottom"}>
+            <div className="flex flex-col gap-2">
+                <h1 className="font-heading text-lg">What is this?</h1>
+              <p className="text-sm text-neutral-600 font-sans">PlumURL makes it easy to interact with charts by letting you choose your search parameters.</p>
+              <p className="text-sm text-neutral-600 font-sans">Choose your incrementation and how far back you want to search.</p>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+        
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button variant="outline">Past Year</Button>
+        <Button variant="outline">Quarterly</Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button variant="outline">Snooze</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger render={<Button variant="outline" size="icon" aria-label="More Options"><MoreHorizontalIcon /></Button>} />
+          <DropdownMenuContent align="end" className="w-40">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <MailCheckIcon />
+                Mark as Read
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <ArchiveIcon />
+                Archive
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <ClockIcon />
+                Snooze
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <CalendarPlusIcon />
+                Add to Calendar
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <ListFilterIcon />
+                Add to List
+              </DropdownMenuItem>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <TagIcon />
+                  Label As...
+                </DropdownMenuSubTrigger>
+                <DropdownMenuSubContent>
+                  <DropdownMenuRadioGroup
+                    value={label}
+                    onValueChange={setLabel}
+                  >
+                    <DropdownMenuRadioItem value="personal">
+                      Personal
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="work">
+                      Work
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="other">
+                      Other
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem variant="destructive">
+                <Trash2Icon />
+                Trash
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </ButtonGroup>
+    </ButtonGroup>
+        
+
+
+
+            {["allTimeClicks"].map((key) => {
+                const chart = key as keyof typeof chartConfig
+                return (
+                <button
+                    key={chart}
+                    data-active={activeChart === chart}
+                    className="flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l data-[active=true]:bg-muted/50 sm:border-t-0 sm:border-l sm:px-8 sm:py-6"
+                    onClick={() => setActiveChart(chart)}
+                >
+                    <span className="text-xs text-muted-foreground">
+                    {chartConfig[chart].label}
+                    </span>
+                    <span className="text-lg leading-none font-bold sm:text-3xl">
+                    {total[key as keyof typeof total].toLocaleString()}
+                    </span>
+                </button>
+                )
+            })}
+            </div>
+        </CardHeader>
+        <CardContent className="px-2 sm:p-6">
+            <ChartContainer
+            config={chartConfig}
+            className="aspect-auto h-[250px] w-full"
+            >
+            <LineChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                left: 12,
+                right: 12,
+                }}
+            >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                minTickGap={32}
+                tickFormatter={(value) => {
+                    const date = new Date(value)
+                    return date.toLocaleDateString("en-US", {
+                    month: "short",
                     })
-                  }}
+                }}
                 />
-              }
-            />
-            <Line
-              dataKey={activeChart}
-              type="monotone"
-              stroke={`var(--color-${activeChart})`}
-              strokeWidth={2}
-              dot={false}
-            />
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
-  )
-}
+                <ChartTooltip
+                content={
+                    <ChartTooltipContent
+                    className="w-[150px]"
+                    nameKey="views"
+                    labelFormatter={(value) => {
+                        return new Date(value).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                        })
+                    }}
+                    />
+                }
+                />
+                <Line
+                dataKey={activeChart}
+                type="monotone"
+                stroke={`var(--color-${activeChart})`}
+                strokeWidth={2}
+                dot={false}
+                />
+            </LineChart>
+            </ChartContainer>
+        </CardContent>
+        </Card>
+    )
+    }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,9 +6,22 @@ import DashboardMenu from "@/components/dashboard-menu";
 import { KPIAllTimeClicks } from "./kpi-all-time-clicks";
 import { timeout } from "@/lib/timeout-test";
 import { useEffect, useState } from "react";
-
+type KPIAllTimeClicksConfig = {
+  date: string,
+  allTimeClicks: number
+}
+type DashboardDataConfig = {
+  allTimeClicks: KPIAllTimeClicksConfig[]
+}
 
 export default function Dashboard() {
+
+  const formatDate = (d: Date): string => {
+      const year = d.getFullYear()
+      const month = String(d.getMonth() + 1).toString()
+      const day = String(d.getDate().toString())
+      return `${year}-${month}-${day}`
+    }
 
 
   //Getting data for dashboard
@@ -19,6 +32,80 @@ export default function Dashboard() {
 
   //Create a KPIAllTimeClicks object and feed it into the first component
 
+  const createAllTimeClicksData1Year = () => { //Creates AllTimeClicks Data for global dashboard data state
+    const today = new Date()
+    const data: KPIAllTimeClicksConfig[] = Array.from({ length: 13 }, (_, i) => {
+      const offset = 12 - i
+      const date = new Date(today.getFullYear(), today.getMonth() - offset, 1)
+      return {
+        date: formatDate(date),
+        allTimeClicks: 0
+      }
+    })
+      return data
+  }
+
+
+
+  const createAllTimeClicksDataQuarterly = () => {
+    const today = new Date();
+    const data: KPIAllTimeClicksConfig[] = Array.from({ length: 13 }, (_, i) => {
+      const offset = (4 - i) * 3;
+      const date = new Date(today.getFullYear(), today.getMonth() - offset, 1);
+      return {
+        date: formatDate(date),
+        allTimeClicks: 0,
+      };
+    });
+    return data;
+  };
+
+  const createAllTimeClicksDataCustom = (
+  startDate: string,
+  endDate: string,
+  stepInMonths: number
+) => {
+  const parseDate = (dateString: string): Date => {
+    const [year, month, day] = dateString.split("-").map(Number);
+    return new Date(year, month - 1, day);
+  };
+
+  const start = parseDate(startDate);
+  const end = parseDate(endDate);
+
+  const data: KPIAllTimeClicksConfig[] = [];
+  let current = new Date(
+    start.getFullYear(),
+    start.getMonth(),
+    start.getDate()
+  );
+
+  while (current <= end) {
+    data.push({
+      date: formatDate(current),
+      allTimeClicks: 0,
+    });
+
+    current = new Date(
+      current.getFullYear(),
+      current.getMonth() + stepInMonths,
+      current.getDate()
+    );
+  }
+
+  return data;
+};
+
+  
+
+  const [dashboardData, setDashboardData] = useState<DashboardDataConfig>({
+    allTimeClicks: createAllTimeClicksDataCustom("2024-10-5", "2025-10-5", 2)
+  })
+
+
+  const getDashboardData = () => { //This function gets the dashboard data for the whole dashboard.
+    console.log(dashboardData)
+  }
 
 
 
@@ -31,6 +118,8 @@ export default function Dashboard() {
       })
     }
     toggleLoading()
+    getDashboardData()
+
   }, [])
 
   return(
@@ -41,11 +130,10 @@ export default function Dashboard() {
           <div className="grid auto-rows-min gap-4 h-full md:grid-cols-2"><DashboardCardSkeleton /><div className="flex flex-row md:flex-row h-full gap-4"><DashboardCardSkeletonAlt /><DashboardCardSkeletonAlt /></div></div>
           <div className="grid grid-cols-3 gap-4 md:grid-cols-6"><DashboardCardSkeleton /><DashboardCardSkeleton /><DashboardCardSkeleton /><DashboardCardSkeleton /><DashboardCardSkeleton /><DashboardCardSkeleton /></div>
         </DashboardMenu> //Skeleton End
-        : 
+        :   
         <DashboardMenu route={"/dashboard"}>
 
-          
-          <div><KPIAllTimeClicks data={null} /></div>
+          <div><KPIAllTimeClicks chartData={dashboardData.allTimeClicks} /></div>
 
         </DashboardMenu>}
       </>
